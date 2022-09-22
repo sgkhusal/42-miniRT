8402a557e22a540fb72d576170bb82c90ba95fa5
sgkhusal <sheela.khusal.08@gmail.com>
2022-09-22 19:09:22 -0300

    Add steps

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:58:57 by sguilher          #+#    #+#             */
-/*   Updated: 2022/09/22 17:00:59 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/09/22 19:08:12 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int argc, char *argv[])
 {
 	t_mlx	mlx;
 
+	// handle_input - check if is valid input and transform_input
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -23,6 +24,8 @@ int	main(int argc, char *argv[])
 	}
 	else
 		printf("file name = %s\n", argv[1]);
+	// ray tracing algorithm
+	// mlx plot and mlx hook
 	mlx.ptr = NULL;
 	mlx.ptr = mlx_init();
 	if (mlx.ptr == NULL)
