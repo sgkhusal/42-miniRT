1f78dbda06784beb5aeb97dd417d3df584814396
sgkhusal <sheela.khusal.08@gmail.com>
2022-10-03 19:07:43 -0300

    Separate main function into handle_input and create_mlx_window functions. Create handle_input file. Add mlx_image creation

--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:59:26 by sguilher          #+#    #+#             */
-/*   Updated: 2022/09/22 17:35:44 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/10/03 19:04:07 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,33 @@
 # include <./libraries/minilibx_linux/mlx.h> // minilibx
 # include <stdio.h> // printf
 
+# define HEIGHT 1000
+# define WIDTH 500
+
+typedef struct s_image
+{
+	void	*ptr;
+	char	*addr;
+	int		bpp;
+	int		line_size;
+	int		endian;
+}				t_image;
+
 typedef struct s_mlx
 {
 	void	*ptr;
 	void	*window;
 	int		width;
 	int		height;
+	t_image	img;
 }				t_mlx;
+
+typedef union u_color
+{
+	int		color;
+	char	rgb[4];
+}				t_color;
+
+void	handle_input(int argc, char *input[]);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,42 +6,57 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:58:57 by sguilher          #+#    #+#             */
-/*   Updated: 2022/09/22 19:08:12 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/10/03 19:04:53 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	create_mlx_window(t_mlx *mlx)
+{
+	mlx->ptr = NULL;
+	mlx->ptr = mlx_init();
+	if (mlx->ptr == NULL)
+	{
+		printf("Error\nmlx_init error\n");
+		exit(EXIT_FAILURE);
+	}
+	mlx->width = 500;
+	mlx->height = 500;
+	mlx->window = mlx_new_window(mlx->ptr, mlx->width, mlx->height, "miniRT");
+	if (mlx->window == NULL)
+	{
+		printf("Error\nmlx_new_window error\n");
+		free(mlx->ptr);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	create_mlx_image(t_mlx *mlx)
+{
+	mlx->img.ptr = mlx_new_image(mlx->ptr, mlx->width, mlx->height);
+	if (mlx->img.ptr == NULL)
+	{
+		printf("Error\nmlx error creating mlx->img.ptr\n");
+		free(mlx->window);
+		free(mlx->ptr);
+		exit(EXIT_FAILURE);
+	}
+	mlx->img.addr = mlx_get_data_addr(mlx->img.ptr, &mlx->img.bpp,
+			&mlx->img.line_size, &mlx->img.endian);
+}
 
 int	main(int argc, char *argv[])
 {
 	t_mlx	mlx;
 
 	// handle_input - check if is valid input and transform_input
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(EXIT_FAILURE);
-	}
-	else
-		printf("file name = %s\n", argv[1]);
+	handle_input(argc, &argv[1]);
+	create_mlx_window(&mlx);
+	create_mlx_image(&mlx);
 	// ray tracing algorithm
 	// mlx plot and mlx hook
-	mlx.ptr = NULL;
-	mlx.ptr = mlx_init();
-	if (mlx.ptr == NULL)
-	{
-		printf("Mlx init error");
-		exit(EXIT_FAILURE);
-	}
-	mlx.width = 500;
-	mlx.height = 500;
-	mlx.window = mlx_new_window(mlx.ptr, mlx.width, mlx.height, "miniRT");
-	if (mlx.window == NULL)
-	{
-		printf("Mlx window error");
-		free(mlx.ptr);
-		exit(EXIT_FAILURE);
-	}
+	mlx_put_image_to_window(mlx.ptr, mlx.window, mlx.img.ptr, 0, 0);
 	mlx_loop(mlx.ptr);
 	return (0);
 }
