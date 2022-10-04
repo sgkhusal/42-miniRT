e67bc15c93ea5e69fc4b315c43646aab85ae815d
sgkhusal <sheela.khusal.08@gmail.com>
2022-10-03 22:29:12 -0300

    Initialize mlx pointers as NULL. Update error messages using minirt_error function

--- src/mlx_utils.c
+++ src/mlx_utils.c
@@ -6,7 +6,7 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 21:17:34 by sguilher          #+#    #+#             */
-/*   Updated: 2022/10/03 21:21:50 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/10/03 22:27:42 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,23 @@
 void	create_mlx_window(t_mlx *mlx)
 {
 	mlx->ptr = NULL;
+	mlx->window = NULL;
+	mlx->img.ptr = NULL;
 	mlx->ptr = mlx_init();
 	if (mlx->ptr == NULL)
-	{
-		printf("Error\nmlx error initializing mlx\n");
-		exit(EXIT_FAILURE);
-	}
-	mlx->width = 500;
-	mlx->height = 500;
+		minirt_error(mlx, "mlx error initializing mlx");
+	mlx->width = WIDTH;
+	mlx->height = HEIGHT;
 	mlx->window = mlx_new_window(mlx->ptr, mlx->width, mlx->height, "miniRT");
 	if (mlx->window == NULL)
-	{
-		printf("Error\nmlx error creating window\n");
-		free(mlx->ptr);
-		exit(EXIT_FAILURE);
-	}
+		minirt_error(mlx, "mlx error creating window");
 }
 
 void	create_mlx_image(t_image *img, t_mlx *mlx)
 {
 	img->ptr = mlx_new_image(mlx->ptr, mlx->width, mlx->height);
 	if (img->ptr == NULL)
-	{
-		printf("Error\nmlx error creating image\n");
-		free(mlx->window);
-		free(mlx->ptr);
-		exit(EXIT_FAILURE);
-	}
+		minirt_error(mlx, "mlx error creating image");
 	img->addr = mlx_get_data_addr(img->ptr, &img->bpp,
 			&img->line_size, &img->endian);
 }
