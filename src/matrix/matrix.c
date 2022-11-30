6bfd4ed04ff0d0638968f04f84a0c9e1b8e01f9b
Eliaris Alvares <likah.alvares@rocketmail.com>
2022-11-30 17:20:56 -0300

    fix: removing auxiliary unused matrix functions

--- src/matrix/matrix.c
+++ src/matrix/matrix.c
@@ -6,7 +6,7 @@
 /*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 12:05:52 by elraira-          #+#    #+#             */
-/*   Updated: 2022/11/30 14:57:21 by elraira-         ###   ########.fr       */
+/*   Updated: 2022/11/30 16:40:25 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,31 +58,6 @@ void	free_matrix(t_matrix m)
 }
 
 /**
- * @brief This function is an auxiliary function to print a given matrix.
- * It is used for debugging purposes.
- *
- * @param m matrix to be printed
- */
-void	print_matrix(t_matrix m) /* remove */
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < m.size)
-	{
-		j = 0;
-		while (j < m.size)
-		{
-			printf("%f ", m.matrix[i][j]);
-			j++;
-		}
-		printf("\n");
-		i++;
-	}
-}
-
-/**
  * @brief This function will create a matrix and then fill it with the
  * identity matrix.
  * identity = 1 0 0 0
@@ -115,34 +90,4 @@ t_matrix	identity_matrix(int size)
 		i++;
 	}
 	return (m);
-}
-
-/**
- * @brief This function will compare two matrices and return 1 if they are
- * identical and 0 if they are different. It is used for debugging purposes.
- *
- * @param m1 first matrix to be compared
- * @param m2 second matrix to be compared
- * @return int 1 if the matrices are identical and 0 if they are different
- */
-int	check_equal_matrices(t_matrix m1, t_matrix m2)
-{
-	int	i;
-	int	j;
-
-	if (m1.size != m2.size)
-		return (FALSE);
-	i = 0;
-	while (i < m1.size)
-	{
-		j = 0;
-		while (j < m1.size)
-		{
-			if (!check_double_values(m1.matrix[i][j], m2.matrix[i][j]))
-				return (FALSE);
-			j++;
-		}
-		i++;
-	}
-	return (TRUE);
 }
