7d97faffc21333533d7df5f177793dd3b5938061
Eliaris Alvares <likah.alvares@rocketmail.com>
2022-11-30 14:33:11 -0300

    adding comments to utils files

--- src/utils/plot.c
+++ src/utils/plot.c
@@ -3,15 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   plot.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:59:31 by sguilher          #+#    #+#             */
-/*   Updated: 2022/11/26 13:06:52 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/11/30 14:32:40 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/**
+ * @brief This function is used to plot a pixel in the canvas given its
+ * coordinates and color.
+ *
+ * @param img the image structure
+ * @param x x coordinate of the pixel
+ * @param y y coordinate of the pixel
+ * @param color color of the pixel
+ */
 static void	put_pixel_color(t_image *img, int x, int y, int color)
 {
 	char	*pixel;
@@ -20,6 +29,15 @@ static void	put_pixel_color(t_image *img, int x, int y, int color)
 	*(unsigned int *)pixel = color;
 }
 
+/**
+ * @brief This function is used to plot the whole image in the mlx window.
+ * It will transform the color vector into an integer color and plot it
+ * using the put_pixel_color function and the mlx_put_image_to_window function.
+ *
+ * @param img the image structure
+ * @param mlx the mlx structure
+ * @param canvas the canvas
+ */
 void	plot_image(t_image *img, t_mlx *mlx, t_vector **canvas)
 {
 	t_color		color;
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -3,15 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 22:09:22 by sguilher          #+#    #+#             */
-/*   Updated: 2022/11/27 18:11:24 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/11/30 14:29:22 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/**
+ * @brief This function will destroy the mlx image, window and display and free
+ * the mlx ptr, clearing the memory.
+ *
+ * @param mlx mlx structure
+ */
 void	clean_minirt(t_mlx *mlx)
 {
 	if (mlx->img.ptr != NULL)
@@ -25,12 +31,29 @@ void	clean_minirt(t_mlx *mlx)
 	}
 }
 
+/**
+ * @brief This function will clean the mlx structure, freeing the allocated
+ * memory and destroying the mlx image, window and display (if it exists).
+ * It will also exit the program with EXIT_SUCCESS status.
+ *
+ * @param mlx mlx structure
+ * @return int EXIT_SUCCESS
+ */
 int	close_minirt(t_mlx *mlx)
 {
 	clean_minirt(mlx);
 	exit(EXIT_SUCCESS);
 }
 
+/**
+ * @brief This function is an auxiliary function to compare two different
+ * points. It will check each coordinate and return 1 if they are equal, or 0
+ * if they are different.
+ *
+ * @param a point a
+ * @param b point b
+ * @return t_bool 1 if equal, 0 if different
+ */
 t_bool	check_equal_points(t_point a, t_point b)
 {
 	return (
@@ -41,6 +64,15 @@ t_bool	check_equal_points(t_point a, t_point b)
 	);
 }
 
+/**
+ * @brief This is an auxiliary function to check if two double values are
+ * equal. It will check if the difference between the two values is less than
+ * the epsilon value.
+ *
+ * @param a double a
+ * @param b double b
+ * @return t_bool 1 if equal, 0 if different
+ */
 t_bool	check_double_values(double a, double b)
 {
 	if (fabs(a - b) < EPSILON)
@@ -48,6 +80,14 @@ t_bool	check_double_values(double a, double b)
 	return (FALSE);
 }
 
+/**
+ * @brief This function will compare two vectors. It will check each coordinate
+ * and return 1 if they are equal, or 0 if they are different.
+ *
+ * @param a vector a
+ * @param b vector b
+ * @return t_bool 1 if equal, 0 if different
+ */
 t_bool	check_equal_vectors(t_vector a, t_vector b)
 {
 	return (
@@ -58,6 +98,12 @@ t_bool	check_equal_vectors(t_vector a, t_vector b)
 	);
 }
 
+/**
+ * @brief This function will free the allocated memory of a list of objects.
+ * It will free the memory of each object and the list itself (if it exists).
+ *
+ * @param arr the array to be freed
+ */
 void	free_array(char **arr)
 {
 	int	i;
