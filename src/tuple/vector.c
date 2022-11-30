c464e050197ffb63d51331815a4ec4a579328023
Eliaris Alvares <likah.alvares@rocketmail.com>
2022-11-30 13:53:18 -0300

    adding comments to tuple functions

--- src/tuple/vector.c
+++ src/tuple/vector.c
@@ -3,15 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   vector.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 12:08:29 by elraira-          #+#    #+#             */
-/*   Updated: 2022/10/15 12:11:25 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/11/30 12:04:30 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/**
+ * @brief This function will set the values of each coordinate of a vector.
+ * The values are passed as parameters except for the w coordinate, which is
+ * always set to 0., in order to represent a vector.
+ *
+ * @param x x coordinate
+ * @param y y coordinate
+ * @param z z coordinate
+ * @return t_vector resulting vector
+ */
 t_vector	set_vector(double x, double y, double z)
 {
 	t_vector	vector;
@@ -24,10 +34,12 @@ t_vector	set_vector(double x, double y, double z)
 }
 
 /**
- * @brief
+ * @brief This function will calculate the magnitude of a vector. It’s how
+ * far you would travel in a straight line if you were to walk from one end of
+ * the vector to the other.
  *
- * @param vector
- * @return double
+ * @param vector the vector to calculate the magnitude of
+ * @return double the length of the vector
  */
 double	vector_length(t_vector vector)
 {
@@ -37,6 +49,18 @@ double	vector_length(t_vector vector)
 	return (length);
 }
 
+/**
+ * @brief Normalization is the process of dividing a vector by its magnitude.
+ * This will result in a vector with the same direction but a magnitude of 1.
+ * This is useful when you want to know the direction of a vector but not its
+ * length. It is also very important to keep the calculations anchored to a
+ * certain scale. If you don’t normalize your vectors, the resulting rays
+ * could be scaled to a very large or small size, which could cause problems
+ * while rendering.
+ *
+ * @param vector the vector to normalize
+ * @return t_vector the normalized vector
+ */
 t_vector	normalize_vector(t_vector vector)
 {
 	double	length;
@@ -51,6 +75,13 @@ t_vector	normalize_vector(t_vector vector)
 	return (set_vector(x, y, z));
 }
 
+/**
+ * @brief This function is used to negate a vector. Which means to get his
+ * opposite direction.
+ *
+ * @param vector vector to be negated
+ * @return t_vector negated vector
+ */
 t_vector	negative_vector(t_vector vector)
 {
 	return (set_vector(-vector.x, -vector.y, -vector.z));
--- src/tuple/vector_operations.c
+++ src/tuple/vector_operations.c
@@ -3,15 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   vector_operations.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 21:59:33 by sguilher          #+#    #+#             */
-/*   Updated: 2022/10/06 11:56:06 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/11/30 13:48:09 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/**
+ * @brief This function is used to add two vectors.
+ *
+ * @param a first vector to be added
+ * @param b second vector to be added
+ * @return t_vector resulting vector
+ */
 t_vector	add_vectors(t_vector a, t_vector b)
 {
 	t_vector	sum;
@@ -26,6 +33,14 @@ t_vector	add_vectors(t_vector a, t_vector b)
 	return (sum);
 }
 
+/**
+ * @brief This function is used to subtract two vectors. It's like adding a vector
+ * with the opposite direction.
+ *
+ * @param a first vector
+ * @param b vector to be subtracted from the first vector
+ * @return t_vector resulting vector
+ */
 t_vector	subtract_vectors(t_vector a, t_vector b)
 {
 	t_vector	sub;
@@ -40,6 +55,15 @@ t_vector	subtract_vectors(t_vector a, t_vector b)
 	return (sub);
 }
 
+/**
+ * @brief This function is used to multiply a vector by a scalar. A scalar is a
+ * single number that is multiplied by each component of the vector. It is
+ * useful to scale a vector while keeping its direction.
+ *
+ * @param vector vector to be multiplied by the scalar
+ * @param scalar scalar to be multiplied
+ * @return t_vector resulting vector
+ */
 t_vector	multiply_vector_by_scalar(t_vector vector, double scalar)
 {
 	t_vector	product;
@@ -54,6 +78,17 @@ t_vector	multiply_vector_by_scalar(t_vector vector, double scalar)
 	return (product);
 }
 
+/**
+ * @brief The scalar product is also known as the dot product. It is used while
+ * intersecting a ray with an object or calculating shading. The scalar product
+ * takes two vectors and returns a scalar. The scalar product of two vectors is
+ * the sum of the products of their corresponding components.
+ * The smaller the scalar product, the larger the angle between the vectors.
+ *
+ * @param a first vector
+ * @param b second vector
+ * @return double scalar product
+ */
 double	scalar_product(t_vector a, t_vector b)
 {
 	double	scalar;
@@ -64,6 +99,15 @@ double	scalar_product(t_vector a, t_vector b)
 	return (scalar);
 }
 
+/**
+ * @brief With cross product, we can find a vector that is perpendicular to the
+ * two vectors we are multiplying. It is used to calculate the normal vector of a
+ * plane. Order matters when calculating the cross product.
+ *
+ * @param a first vector
+ * @param b second vector
+ * @return t_vector resulting vector
+ */
 t_vector	cross_product(t_vector a, t_vector b)
 {
 	double	x;
