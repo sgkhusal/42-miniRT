3bac8201608d64f435c87e673493f1aac7836645
Eliaris Alvares <likah.alvares@rocketmail.com>
2022-12-04 09:49:35 -0300

    add: input file documentation almost finished

--- src/input/handle_light.c
+++ src/input/handle_light.c
@@ -3,15 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   handle_light.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 13:53:53 by sguilher          #+#    #+#             */
-/*   Updated: 2022/11/29 16:53:30 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/12/04 09:21:05 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/**
+ * @brief This function will validate the information of the light. The first
+ * info is the position of the light, which must be a valid vector. The second
+ * info is the brightness of the light, which must be a double between 0 and 1.
+ * The third info is the color of the light, which must be a valid RGB color
+ * (not mandatory).
+ *
+ * @param infos the information of the light, which is a string
+ * @return int 0 if the information is valid, 1 otherwise
+ */
 static int	validate_light_chars(char **infos)
 {
 	if (validate_coordinates_chars(infos[1]) == ERROR)
@@ -23,6 +33,16 @@ static int	validate_light_chars(char **infos)
 	return (OK);
 }
 
+/**
+ * @brief This function will handle the light information. It will split the
+ * information in the string by the separator ' ', validate the information,
+ * transform the information in the correct type and store it in t_light struct
+ * variables. Then, it will free the information that was already parsed.
+ *
+ * @param line the information of the light
+ * @param light the t_light struct
+ * @return int ERROR if any information is invalid or error occurs, OK otherwise
+ */
 int	handle_light(char *line, t_light *light)
 {
 	char		**infos;
--- src/input/input_utils.c
+++ src/input/input_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   input_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 19:10:01 by sguilher          #+#    #+#             */
-/*   Updated: 2022/11/29 17:48:12 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/12/04 09:42:07 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /**
  * @brief the atod() function converts a string to a double.
- * 
+ *
  * @param nb string to be converted
  * @return double converted string
  */
@@ -37,9 +37,10 @@ double	ft_atod(char *nb)
 }
 
 /**
- * @brief will parse the input file by splitting the string by the delimiter (',').
- * Then, it will check if the file contains exactly the number of elements specified in the subject.
- * 
+ * @brief will parse the input file by splitting the string by the delimiter
+ * (','). Then, it will check if the file contains exactly the number of
+ * elements specified in the subject.
+ *
  * @param line line to be parsed
  * @param c  delimiter
  * @param size_expected number of elements expected
